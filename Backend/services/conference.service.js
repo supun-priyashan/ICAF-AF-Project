a804a20f450bc9cfd912fa7a6059c6765bbe93f1
Dilitha Pandigamage <dilitha.pandigamge@acentura.com>
2021-06-27 05:34:41 +0530

    conference service file updated.

--- Backend/services/conference.service.js
+++ Backend/services/conference.service.js
@@ -1,6 +1,7 @@
 const Conference = require("../models/conference.model");
 
 const getConference = async (request, response) => {
+
     try {
         const conference = await Conference.find();
         response.status(200).json({ conference: conference });
