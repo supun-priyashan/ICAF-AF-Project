7b614bd248c52574f0b440741c790b9276c2cae1
Dilitha Pandigamage <dilitha.pandigamge@acentura.com>
2021-06-30 21:15:37 +0530

    Landing page update 1.

--- Frontend/src/components/views/LandingPage/LandingPage.js
+++ Frontend/src/components/views/LandingPage/LandingPage.js
@@ -11,16 +11,25 @@ function LandingPage(props) {
                     <div className='container'>
                         <div className='row'>
                             <div className='col-md-8 col-md-offset-2 intro-text'>
-                                <h1>
-                                    {props.data ? props.data.title : 'Loading'}
+                                <div className='headertop'>
+                                    International Conference on Application Frameworks
                                     <span></span>
-                                </h1>
-                                <p>{props.data ? props.data.paragraph : 'Loading'}</p>
+                                </div>
+                                <p>But don't take our word for it, here's what past attendees have said:
+                                    ...
+                                    “A great example of a virtual conference that actually felt like a conference, with interaction with real people – not just a bunch of webinars to watch. Kudos to the GOTO team.”
+                                    Robin Moffatt, senior developer advocate at Confluent
+                                    ...
+                                    “The combination of topics, sessions and speakers is really great. It is the essence of GOTO in an online format!”
+                                    Michael Repplinger, team leader at Dillinger
+                                    ...
+                                    "Incredible job by the team at GOTO for creating an immersive, educational and fun remote conference experience!"
+                                    Garrett Smith, founder at Guild AI</p>
                                 <a
                                     href='#features'
                                     className='btn btn-custom btn-lg page-scroll'
                                 >
-                                    Learn More
+                                    Register Now
                                 </a>{' '}
                             </div>
                         </div>
